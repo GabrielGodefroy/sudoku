9ef754583b4950c791dfa26f0a48bf9f92743e41
GabrielGolbi <gabriel.godefroy@hotmail.fr>
2020-10-08 21:07:19 +0200

    Update tests

--- cpp/tests/t_sudoku.cpp
+++ cpp/tests/t_sudoku.cpp
@@ -1,4 +1,3 @@
-#define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 
 #include <sudoku/grid.hpp>
@@ -40,4 +39,65 @@ TEST_CASE("Check for box index", "[sudoku9x9]")
     REQUIRE(grid.get_box(7, 8) == 8);
     REQUIRE(grid.get_box(8, 7) == 8);
     REQUIRE(grid.get_box(8, 8) == 8);
+}
+
+TEST_CASE("Check read an empty sudoku", "[sudoku9x9]")
+{
+    SudokuGrid grid(std::string(81, '0'));
+
+    REQUIRE(grid(0, 0) == 0);
+    REQUIRE(grid(1, 0) == 0);
+    REQUIRE(grid(0, 2) == 0);
+    REQUIRE(grid(8, 8) == 0);
+}
+
+TEST_CASE("Check read a fake sudoku", "[sudoku9x9]")
+{
+    SudokuGrid grid(std::string(81, '9'));
+
+    REQUIRE(grid(0, 0) == 9);
+    REQUIRE(grid(1, 0) == 9);
+    REQUIRE(grid(0, 2) == 9);
+    REQUIRE(grid(8, 8) == 9);
+}
+
+TEST_CASE("Check read a partially defined sudoku", "[sudoku9x9]")
+{
+    SudokuGrid grid(
+        "1	2	3	4	5	6	7	8	9"
+        "5	0	0	0	0	0	0	0	0   "
+        "7	0	0	0	000	0	0"
+        "3	0	0	0	0	0	0	0	0"
+        "4	0	0	0	0	0	0	0	0"
+        "9	0	0	0	0	0	0	0	0"
+        "8	0	0	0	0	0	0	0	0"
+        "2	0	0	0	0	0	0	0	0"
+        "600000009");
+    REQUIRE(grid(0, 0) == 1);
+    REQUIRE(grid(1, 0) == 5);
+    REQUIRE(grid(0, 2) == 3);
+    REQUIRE(grid(8, 8) == 9);
+}
+
+
+TEST_CASE("Load an empty sudoku from a file", "[sudoku9x9]")
+{
+    SudokuGrid grid = SudokuGrid::load_from_file("../../DATA/empty.txt");
+
+    REQUIRE(grid(0, 0) == 0);
+    REQUIRE(grid(1, 0) == 0);
+    REQUIRE(grid(0, 2) == 0);
+    REQUIRE(grid(8, 8) == 0);
+}
+
+TEST_CASE("Load a partial sudoku from a file", "[sudoku9x9]")
+{
+    SudokuGrid grid = SudokuGrid::load_from_file("../../DATA/easy.txt");
+
+    REQUIRE(grid(0, 0) == 9);
+    REQUIRE(grid(1, 0) == 0);
+    REQUIRE(grid(2, 0) == 8);
+    REQUIRE(grid(8, 6) == 5);
+    REQUIRE(grid(8, 7) == 7);
+    REQUIRE(grid(8, 8) == 0);
 }
